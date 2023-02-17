518bb632ae5788e59eba0dbd32d0951c2d9e52ab
Minh Huynh <mhuynh@Minhs-MBP-2.attlocal.net>
2023-02-17 11:44:11 -0800

    add business endpoints, rename pages to views

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
+app.use(express.static(__dirname + "/views"));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("/businesses", (req, res) => {
+	res.send("This is a listing page.");
+});
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+	console.log("Server listening on port 3000");
 });
