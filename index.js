148e1f2ce94ba4f54c5c1b02cd459ba89680ca41
Kevin Garcia <kevin.garciajulca@outlook.com>
2023-02-24 15:57:20 -0800

    Initial DB setup

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
+
+
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017/mydb';
+
+const client = new MongoClient(uri);
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log('Failed to connect to MongoDB', err);
+  }
+}
+
+connect();
