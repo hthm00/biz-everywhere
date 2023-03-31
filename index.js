1509070ea4b5a3efc6620d69abbcca371d257b7d
y0shiki <63571264+y0shiki@users.noreply.github.com>
2023-03-30 20:02:17 -0700

    update index.js for customizing sass

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
+//engine setup
 app.set('view engine', 'ejs');
 app.set('views', './views/pages');
+
+//app configuration 
+//app.use(express.static(path.join(__dirname, "js")));
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+
 
 app.get('/', (req, res) => {
     // res.send('Hello, World!');
     res.render('view');
   });
+
+
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
