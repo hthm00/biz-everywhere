9daa719ea5633eee3cd8cefd17d191a7a442bc42
Kevin Garcia <kevin.garciajulca@outlook.com>
2023-05-05 16:04:31 -0700

    Update business.js

--- models/business.js
+++ models/business.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const express = require('express');
+const router = express.Router();
+
 const Schema = mongoose.Schema;
 
 //Creates the Business Schema
@@ -106,3 +109,50 @@ const businessSchema = new Schema({
 
 
 module.exports = mongoose.model('Business', businessSchema);
+
+
+router.get('/', (req, res) => {
+  Business.find().then((businesses) => {
+    res.json(businesses);
+  }).catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
+
+router.post('/', (req, res) => {
+  const business = new Business(req.body);
+  business.save().then((business) => {
+    res.json(business);
+  }).catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Business.findById(req.params.id).then((business) => {
+    res.json(business);
+  }).catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  Business.findByIdAndUpdate(req.params.id, req.body).then(() => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Business.findByIdAndDelete(req.params.id).then(() => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
